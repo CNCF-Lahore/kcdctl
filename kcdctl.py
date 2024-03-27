a81b476c2736531d88ec2b4e9853e8230618ffed
Adil Shehzad <adil.shehzad4146@gmail.com>
2024-03-27 10:30:29 +0000

    Adding OS dependency code

--- kcdctl.py
+++ kcdctl.py
@@ -7,10 +7,37 @@ from LabManager import LabManager
 from determineOS import getOS
 from DependencyInsScripts.WindowsSetup import *
 
+def detOSandInsDep():
+    osName, osDetails, osVersion, osFlavor = getOS()
+    print(f"The operating system is: {osName}")
+    print(f"The operating system details are: {osDetails}")
+    print(f"The operating system version is: {osVersion}")
+    if osFlavor:
+        print(f"The operating system flavor is: {osFlavor}")
+    if 'windows' in osDetails.lower():
+        print("Running Windows Installation scripts")
+        WindowsSoftwareInstaller.main()
+    elif 'linux' in osDetails.lower():
+        print("Running Linux Installation scripts")
+        LinuxInstaller.main()
+    elif 'darwin' in osDetails.lower():  # Check for macOS
+        print("Running macOS Installation scripts")
+        MacOSInstaller.main()
+
 def parse_args():
     parser = argparse.ArgumentParser(description='Manage Kubernetes cluster deployment.')
-    parser.add_argument('--command', choices=['deploy', 'delete', 'quit', 'help'], help='The command to execute.')
-    parser.add_argument('lab_id', nargs='?', help='The ID of the lab to deploy or delete.')
+    subparsers = parser.add_subparsers(dest='command', help='Sub-command help')
+
+    # Adding subparsers for each command
+    parser_deploy = subparsers.add_parser('deploy', help='Deploy a lab environment based on its ID.')
+    parser_deploy.add_argument('lab_id', help='The ID of the lab to deploy.')
+
+    parser_delete = subparsers.add_parser('delete', help='Clean up the deployed lab environment based on its ID.')
+    parser_delete.add_argument('lab_id', help='The ID of the lab to delete.')
+
+    subparsers.add_parser('quit', help='Exit the application.')
+    subparsers.add_parser('help', help='Display the help message.')
+
     return parser.parse_args()
 
 def main():
@@ -33,13 +60,12 @@ def main():
             print(f"No configuration found for lab '{args.lab_id}'.")
     elif args.command == 'quit':
         print("Exiting...")
-        return
-    elif args.command == 'help' or not args.command:
+    elif args.command == 'help' or args.command is None:
         # Display help message
         print("""
 Available Commands:
-  deploy [lab_id]     - Deploy a lab environment based on its ID. Previous environments will be cleaned up. Example: --command deploy --lab_id lab2
-  delete [lab_id]    - Clean up the deployed lab environment based on its ID. Example: --command delete --lab_id lab2
+  deploy [lab_id]     - Deploy a lab environment based on its ID. Previous environments will be cleaned up. Example: deploy lab2
+  delete [lab_id]    - Clean up the deployed lab environment based on its ID.
   quit               - Exit the application.
   help               - Display this help message.
 """)
